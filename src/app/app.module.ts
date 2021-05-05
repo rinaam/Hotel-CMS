72bd25c54c4fcf81ba0800743e90999eda9a87ae
Marina <marina.savic90@gmail.com>
2021-05-05 13:09:39 +0200

    angular firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB9ioft3QCDm5PDlQIzl0al8qLhkxOnt6M',
+    authDomain: 'hotel-cms-bfefc.firebaseapp.com',
+    projectId: 'hotel-cms-bfefc',
+    storageBucket: 'hotel-cms-bfefc.appspot.com',
+    messagingSenderId: '223973318056',
+    appId: '1:223973318056:web:5d6c3a26dccd3b124f919d',
+  },
 };
 
 /*
